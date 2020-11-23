9b8ff3e15b3b00e31c5f4c1ec0706a1fb080de14
Munze <sasa.oravec@gmail.com>
2020-11-23 17:03:41 +0100

    Initial commit - lection 77

---
+++ src/utils/getUserId.js
@@ -0,0 +1,23 @@
+import jwt, { verify } from 'jsonwebtoken'
+
+
+const getUserId = (request, requireAuth = true) => {
+    const header = request.request.headers.authorization
+
+    if (header) {
+        const token = header.replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'thisisasecret')
+
+        return decoded.userId
+
+    }
+
+    if(requireAuth) {
+        throw new Error('Authentication required')
+    }
+
+    return null
+
+}
+
+export { getUserId as default }
